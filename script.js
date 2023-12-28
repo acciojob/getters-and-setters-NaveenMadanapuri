9cd443550d56076d1b9c02816e2f960303d873cd
NaveenMadanapuri <139567313+NaveenMadanapuri@users.noreply.github.com>
2023-12-28 19:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,20 +33,7 @@ class Teacher extends Person {
     console.log(`${this.name} is teaching.`);
   }
 }
-const person = new Person("John Doe", 25);
-console.log(person.name); // Output: John Doe
-person.age = 26; // Using the setter
 
-// Creating a Student
-const student = new Student("Alice Smith", 20);
-console.log(student.name); // Output: Alice Smith
-student.study(); // Output: Alice Smith is studying.
-
-// Creating a Teacher
-const teacher = new Teacher("Professor Johnson", 40);
-console.log(teacher.name); // Output: Professor Johnson
-teacher.teach(); // Output: Professor Johnson is teaching.
-// Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
