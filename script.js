a9f43d400cfc544bfc0c2043130466707430bd3f
NaveenMadanapuri <139567313+NaveenMadanapuri@users.noreply.github.com>
2023-12-28 19:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
 //complete this code
-class Person {}
+class Person {
+	 constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(age) {
+    this._age = age;
+  }
+}
 
+class Student extends Person {
+	constructor(name, age) {
+    super(name, age);
+  }
+
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+	constructor(name, age) {
+    super(name, age);
+  }
+
+  teach() {
+    console.log(`${this.name} is teaching.`);
+  }
+}
+const person = new Person("John Doe", 25);
+console.log(person.name); // Output: John Doe
+person.age = 26; // Using the setter
+
+// Creating a Student
+const student = new Student("Alice Smith", 20);
+console.log(student.name); // Output: Alice Smith
+student.study(); // Output: Alice Smith is studying.
+
+// Creating a Teacher
+const teacher = new Teacher("Professor Johnson", 40);
+console.log(teacher.name); // Output: Professor Johnson
+teacher.teach(); // Output: Professor Johnson is teaching.
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
